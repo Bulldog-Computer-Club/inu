bd1aa98f9e48990c873c02e084f8297c9bb4183f
AndreiSva <andrei@sdf.org>
2022-03-29 23:49:27 -0700

    chore: proper entry point

--- bot.py
+++ bot.py
@@ -5,18 +5,16 @@ import discord
 import dotenv
 
 dotenv.load_dotenv()
-
 client = discord.Client()
-
+token = os.getenv("DISCORD_TOKEN")
 
 @client.event
 async def on_ready():
     print(f"Successfully logged in as {client.user}")
 
+if __name__ == "__main__":
+    if not token:
+        print("required environmental variable DISCORD_TOKEN not set")
+        sys.exit(1)
 
-token = os.getenv("DISCORD_TOKEN")
-if not token:
-    print("required environmental variable DISCORD_TOKEN not set")
-    sys.exit(1)
-
-client.run(token)
+    client.run(token)
