db9e5163d04ed579bd6e15bff229ab2387af835a
AndreiSva <andrei@sdf.org>
2022-03-30 23:23:47 -0700

    feat: add --quiet flag

--- bot.py
+++ bot.py
@@ -26,9 +26,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--token", help="specify a Discord bot token")
     parser.add_argument("--debug", help="set the debug loglevel", action="store_true")
+    parser.add_argument("--quiet", help="silence everything except errors", action="store_true")
     args = parser.parse_args()
     if args.debug:
         logging.getLogger().setLevel(logging.DEBUG)
+    elif args.quiet:
+        logging.getLogger().setLevel(logging.ERROR)
 
     token: Optional[str] = args.token or os.getenv("DISCORD_TOKEN")
     if not token:
