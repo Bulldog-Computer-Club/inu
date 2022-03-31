0dec0108f088beb4f78b49618a3c1ecf3448668f
Joe <56809242+Jo3-L@users.noreply.github.com>
2022-03-30 17:55:17 -0700

    refactor(bot): simplify token handling logic

--- bot.py
+++ bot.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python3
 
-import os
-import sys
 import argparse
 import logging
+import os
+import sys
+from typing import Optional
 
 import discord
 import dotenv
 
 dotenv.load_dotenv()
 client = discord.Client()
-token = os.getenv("DISCORD_TOKEN")
 
-logging.basicConfig(format="%(asctime)s [%(levelname)s]: %(message)s", level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s [%(levelname)s]: %(message)s", level=logging.INFO
+)
+
 
 @client.event
 async def on_ready():
     logging.info(f"successfully logged in as {client.user}")
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--token", help="specify a Discord bot token")
     args = parser.parse_args()
-    if args.token not None:
-        token = args.token
+    token: Optional[str] = args.token or os.getenv("DISCORD_TOKEN")
     if not token:
         logging.error("no token specified, use --token or set DISCORD_TOKEN")
         sys.exit(1)
