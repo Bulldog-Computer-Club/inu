b1121d23f289326e4e3127f771f0c21d1847034a
AndreiSva <andrei@sdf.org>
2022-03-30 17:15:53 -0700

    fix: token not read from .env

--- bot.py
+++ bot.py
@@ -12,19 +12,20 @@ dotenv.load_dotenv()
 client = discord.Client()
 token = os.getenv("DISCORD_TOKEN")
 
-logging.basicConfig(format="%(asctime)s [%(levelname)s]: %(message)s")
+logging.basicConfig(format="%(asctime)s [%(levelname)s]: %(message)s", level=logging.INFO)
 
 @client.event
 async def on_ready():
-    print(f"Successfully logged in as {client.user}")
+    logging.info(f"successfully logged in as {client.user}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--token", help="specify a Discord bot token")
     args = parser.parse_args()
-    token = args.token
     if not token:
-        logging.error("no token specified, use --token or set DISCORD_TOKEN")
-        sys.exit(1)
+        token = args.token
+        if not token:
+            logging.error("no token specified, use --token or set DISCORD_TOKEN")
+            sys.exit(1)
 
     client.run(token)
