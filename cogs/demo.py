50124de1cfb393102642f5127b869b2c306971aa
Joe <56809242+Jo3-L@users.noreply.github.com>
2022-06-24 13:21:15 -0700

    feat(demo-cog): add rps command
    Also cleanup the existing code, while we're at it.

--- cogs/demo.py
+++ cogs/demo.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 import discord
-import asyncio
+import random
 
 
 class DemoCog(commands.Cog):
@@ -9,43 +9,65 @@ class DemoCog(commands.Cog):
 
     @commands.command()
     async def ping(self, ctx: commands.Context):
+        """Check if the bot is online."""
         await ctx.send("Pong!")
 
     @commands.command()
     async def embed(self, ctx: commands.Context, title: str, desc: str):
-        # if the title or desc is empty, send a message saying its empty
-        if not title or not desc:
-            await ctx.send("Title or description is empty!")
-            return
-
-        # create a discord embed with the first arg being its title, and the rest concatenated for its content
+        """Create a simple embed with a title and description."""
         embed = discord.Embed(title=title, description=desc)
-
-        # send the embed
         await ctx.send(embed=embed)
 
     @commands.command()
-    async def reaction(self, ctx: commands.Context, emoji: str):
-
+    async def react(self, ctx: commands.Context, emoji: str):
+        """React with an emoji."""
         try:
-            await ctx.message.add_reaction(emoji);
-        except discord.HTTPException as e:
-            await ctx.send(e)
+            await ctx.message.add_reaction(emoji)
+        except discord.HTTPException:
+            await ctx.send("Not a valid emoji.")
 
     @commands.command()
+    @commands.has_permissions(manage_messages=True)
     async def clear(self, ctx: commands.Context, amount: int):
-        await ctx.channel.purge(limit=amount + 1)
-        msg = await ctx.send("Purged {} messages!".format(amount))
+        """Delete messages from the channel in bulk."""
+        limit = amount + 1  # also delete triggering message
+        if limit > 100:
+            await ctx.send("The amount of messages to delete must be less than 100.")
+        elif amount <= 0:
+            await ctx.send("The amount of messages to delete must be greater than 0.")
 
-        await asyncio.sleep(1)
-        await msg.delete()
+        await ctx.channel.purge(limit=limit)
+        msg = await ctx.send(f"Purged {amount} messages!")
+        await msg.delete(delay=1)
 
     @commands.command()
+    @commands.has_permissions(manage_roles=True)
     async def assign(self, ctx: commands.Context, role: discord.Role):
+        """Assign a role to yourself."""
         await ctx.author.add_roles(role)
-        await ctx.send("You have been assigned the role {}!".format(role.name))
+        await ctx.send(f"You have been assigned the role {role.name}!")
 
-    
+    @commands.command()
+    async def rps(self, ctx: commands.Context, choice: str):
+        """Play rock-paper-scissors with the bot."""
+        CHOICES = ("rock", "paper", "scissors")
+        WINNING_PAIRS = (("rock", "scissors"), ("scissors", "paper"), ("paper", "rock"))
+
+        user_choice = choice.lower()
+        if user_choice in CHOICES:
+            bot_choice = random.choice(CHOICES)
+            if bot_choice == user_choice:
+                await ctx.send(f"I chose {user_choice} too, so it appears we're tied!")
+            elif (user_choice, bot_choice) in WINNING_PAIRS:
+                await ctx.send(
+                    f"You chose {user_choice}, which beats my choice of {bot_choice}, so it appears you win."
+                )
+            else:
+                await ctx.send(
+                    f"I chose {bot_choice}, and that beats your choice of {user_choice}. Good game!"
+                )
+        else:
+            await ctx.send("Choose one of rock, paper, or scissors.")
 
 
 async def setup(bot: commands.Bot):
